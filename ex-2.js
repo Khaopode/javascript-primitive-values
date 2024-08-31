c2f0f5c071cc6341127ab6acb6299b08b8adc240
Khaopode <cjintate@gmail.com>
2024-08-31 15:01:45 +0700

    bp5-ChanunyaJ

--- ex-2.js
+++ ex-2.js
@@ -5,3 +5,7 @@ let productPrice = 20000;
 let productDiscount = 0.3;
 
 // Start coding here
+let productNewPrice = (1-productDiscount) * productPrice ;
+let productImage = `${productName} is now on discount from THB ${productPrice} to THB ${productNewPrice}. Get it now!`;
+
+console.log(productImage);
--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let productPrice = 27000;
+let productDiscount = 0.25;
+let productNewPrice = productPrice * (1-productDiscount) ; 
+console.log (productNewPrice);
